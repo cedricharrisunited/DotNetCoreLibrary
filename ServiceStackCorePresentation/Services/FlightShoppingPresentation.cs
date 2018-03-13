c722c95311fe76a5e9d8cdd9ee48c6b807c9b627
Cedric <cedric.harris@united.com>
2018-03-13 15:08:11 -0500

    TRST

---
+++ ServiceStackCorePresentation/Services/FlightShoppingPresentation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ServiceStack;
+using Funq;
+using DOTNETCoreProxy = DotNetCore.United.Services.FlightShopping.Common;
+
+
+namespace ServiceStackCorePresentation.Services
+{
+
+    [Route("/FlightShopping", Verbs = "POST")]
+    public class ShopRequest : DOTNETCoreProxy.ShopRequest, IReturn<ShopResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class ShopResponse
+    {
+        public string Result { get; set; }
+    }
+
+
+    public class FlightShoppingPresentation : Service
+    {
+        public object Post(ShopRequest request)
+        {
+            System.Random rnd = new System.Random();
+            int rndValue = rnd.Next(0,10);
+
+            return new ShopResponse { Result = $"Hello, {request.Name}  this your request number : {rndValue}!" };
+        }
+    }
+}
