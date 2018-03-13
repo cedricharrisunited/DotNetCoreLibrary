21b71ded458a158893b14dfb824d8f4f49c99f1f
Cedric <cedric.harris@united.com>
2018-03-13 14:58:41 -0500

    TRST

---
+++ DotNetCoreLibrary/DotNetCoreLibraryProxy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DOTNET = United.Services.FlightShopping.Common;
+
+namespace DotNetCore.United.Services.FlightShopping.Common
+{
+    public class ShopRequest : DOTNET.ShopRequest
+    {
+        public ShopRequest() {}
+    }
+}
---
+++ ServiceStackCorePresentation/AppHost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ServiceStack;
+using Funq;
+using ServiceStackCorePresentation.Services;
+using ServiceStack.Configuration;
+using ServiceStack.Api.Swagger;
+
+
+
+namespace ServiceStackCorePresentation
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base(nameof(ServiceStackCorePresentation), typeof(FlightShoppingPresentation).Assembly)
+        {
+            var appSettings = new EnvironmentVariableSettings();
+            ServiceStack.Licensing.RegisterLicense(appSettings.Get<string>("ServiceStackLicenseKey", string.Empty));
+        }
+
+        public override void Configure(Container container)
+        {           
+            this.Plugins.Add(new SwaggerFeature());
+
+        }
+
+    }
+}
